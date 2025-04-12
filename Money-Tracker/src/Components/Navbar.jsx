3f0e6f9e252f3d534086784913a48d62420f2e2c
chiragahir20 <chiragchovatiya2005@gmail.com>
2025-04-12 09:42:37 +0530

    Input

--- Money-Tracker/src/Components/Navbar.jsx
+++ Money-Tracker/src/Components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Input from "./input";
 
 function Navbar(){
     return(
         <>
-            <div className="pages" style={{margin:0,padding:"100px 25px",backgroundColor:"#0B0B0B",display:"flex",flexDirection:"column",width:"12vw",height:"100vh", justifyContent:"start",lineHeight:"2"}}>
+            <div className="pages" style={{margin:0,padding:"100px 25px",backgroundColor:"#0B0B0B",display:"flex",flexDirection:"column",width:"12%",height:"100vh", justifyContent:"start",lineHeight:"2"}}>
                 <div className="home" style={{color:"white",fontSize:"24px"}}>Home</div>
                 <div className="input" style={{color:"white",fontSize:"24px"}}>Input</div>
                 <div className="profile" style={{color:"white",fontSize:"24px"}}>Profile</div>
